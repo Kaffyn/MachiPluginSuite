e07bd300aeb93fb1914587ee6cbeed97fe3a7bfa
Kaffyn <kaffyn.tech@gmail.com>
2025-12-13 22:02:06 -0300

    a

--- addons/memento/src/memento_manager.h
+++ addons/memento/src/memento_manager.h
@@ -14,6 +14,17 @@ protected:
 public:
 	MementoManager();
 	~MementoManager();
+
+    // Persist a node and its children recursively to a Dictionary/Resource
+    void save_level_state(Node *p_level_root);
+    
+    // Load state back into a node tree
+    void load_level_state(Node *p_level_root);
+    
+    Dictionary get_last_saved_state() const { return last_state; }
+    
+private:
+    Dictionary last_state;
 };
 
 #endif // MEMENTO_MANAGER_H
